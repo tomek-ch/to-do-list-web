80ce19bb19470b80923c70859e4baf5551e921fb
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-05 21:35:17 +0200

    Get rid of material ui warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
+import React from "react";
 
 class MyDocument extends Document {
   render() {
@@ -20,5 +22,25 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
 
 export default MyDocument;
