d3b158302a7742cebe7d88fb766e3f86732fb0f3
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-05 18:48:45 +0200

    Set up material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Container from "@material-ui/core/Container";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container maxWidth="sm">
+      <Component {...pageProps} />
+    </Container>
+  );
 }
-export default MyApp
+export default MyApp;
